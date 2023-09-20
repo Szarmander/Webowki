2731fb1a931df46c2e777680229f277d0d9ccb3e
Szarmander <kubuszrama@gmail.com>
2023-09-20 17:50:33 +0200

    added hello world in TS

---
+++ 2023_09_20/typescriptNode/src/index.ts
@@ -0,0 +1,15 @@
+import { createServer } from "http";
+
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Contenty-Type", "text/html");
+  res.end("Hello worldaaa");
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
