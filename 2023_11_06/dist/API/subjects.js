96ed2a00b4a5b8605170d161f1ac2ec17dd8bfbe
Szarmander <kubuszrama@gmail.com>
2023-11-06 19:28:04 +0100

    added disc files

---
+++ 2023_11_06/dist/API/subjects.js
@@ -0,0 +1,55 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.subjects = void 0;
+exports.subjects = [
+    {
+        id: 1,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 2,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 3,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 4,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 5,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 6,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 7,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 8,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 9,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+    {
+        id: 10,
+        name: "Matma",
+        hoursAWeek: 5
+    },
+];
